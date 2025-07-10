5a9b3e9896de4da66a1126f249dc174364826349
Abhijit <abhijitghadge29@gmail.com>
2025-07-10 15:32:17 -0500

    column added

--- my_dbt_project/models/staging/stg_sales.sql
+++ my_dbt_project/models/staging/stg_sales.sql
@@ -1,6 +1,7 @@
 {{ config(
     materialized = 'incremental',
-    unique_key    = 'sale_id'
+    unique_key    = 'sale_id',
+    incremental_strategy   = 'merge'
 ) }}
 
 select
@@ -11,3 +12,11 @@ select
   sale_amount,
   sale_date
 from {{ ref('sales') }}
+
+  {% if is_incremental() %}
+    -- only pull sales newer than what’s already in the target table
+    where sale_date > (
+      select max(sale_date) 
+      from {{ this }}
+    )
+  {% endif %}
