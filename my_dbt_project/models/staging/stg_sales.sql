f2ccf1182248138d23adbf2171afc05a5ad676d5
Abhijit <abhijitghadge29@gmail.com>
2025-07-10 10:25:37 -0500

    column added

--- my_dbt_project/models/staging/stg_sales.sql
+++ my_dbt_project/models/staging/stg_sales.sql
@@ -1,9 +1,13 @@
+{{ config(
+    materialized = 'incremental',
+    unique_key    = 'sale_id'
+) }}
+
 select
   sale_id,
   customer_id,
   product_id,
   region_id,
   sale_amount,
-  sale_date,
-  sale_quantity
+  sale_date
 from {{ ref('sales') }}
